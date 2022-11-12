7db60e4a003d1ac9d9f51e91730a4c0995abe805
Nucleon <114564783+Nucleon-Technologies@users.noreply.github.com>
2022-11-12 21:34:01 +0530

    Update sex.lua

--- sex.lua
+++ sex.lua
@@ -1,1 +1,1 @@
-messagebox("Nucleon is currently down, Please join our discord server to be Notified on updated. Please wait for Falcon Software 😏","Nucleon",0)
+messagebox("Nucleon","Nucleon is currently down, Please join our discord server to be Notified on updated. Please wait for Falcon Software 😏",0)
